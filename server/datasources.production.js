0ce529aef46b2fa847af3f10aa48cad5733e156c
Peter Horton <phorton@softstackfactory.com>
2020-09-24 18:08:59 -0700

    updated backend db

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,8 +1,13 @@
 'use strict';
+require('dotenv').config()
 
 module.exports = {
   mongo: {
     connector: 'mongodb',
-    url: process.env.DATABASE_PRODUCTION_URL,
+  useNewUrlParser: true,
+    //url:  "mongodb://ssfdev:SSFisGreat!1@ds056009.mlab.com:56009/reboot",
+    url: process.env.DATABASE_PRODUCTION_URL
+   // url: "mongodb+srv://ssfdev:SSFisGreat!@cluster0.dkhwf.mongodb.net/reboot?retryWrites=true&w=majority"
+
   },
 };
